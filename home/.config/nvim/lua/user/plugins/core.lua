77abf95a9a2b028162db7508dd3e6e2c77848453
lumynou5 <lumynou5.tw@gmail.com>
2023-04-13 09:24:54 +0800

    update astronvim user config

--- home/.config/nvim/lua/user/plugins/core.lua
+++ home/.config/nvim/lua/user/plugins/core.lua
@@ -38,7 +38,7 @@ return {
           if node.type == "directory" or node:has_children() then
             if not node:is_expanded() then -- if unexpanded, expand
               state.commands.toggle_node(state)
-            else -- if expanded and has children, seleect the next child
+            else                           -- if expanded and has children, seleect the next child
               require("neo-tree.ui.renderer").focus_node(state, node:get_child_ids()[1])
             end
           else -- if not a directory just open it
@@ -97,6 +97,7 @@ return {
         filesystem = {
           filtered_items = {
             hide_dotfiles = false,
+            hide_gitignored = false,
             hide_by_name = {
               ".git",
             },
