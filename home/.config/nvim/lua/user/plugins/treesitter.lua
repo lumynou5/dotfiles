a9e09ece513328396405bc878837ad02f863df52
Lumynous <lumynou5.tw@gmail.com>
2023-06-19 13:48:21 +0800

    nvim: add zig

--- home/.config/nvim/lua/user/plugins/treesitter.lua
+++ home/.config/nvim/lua/user/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    ensure_installed = { "cmake", "cpp", "javascript", "lua", "python", "typescript", "yaml" },
+    ensure_installed = { "cmake", "cpp", "javascript", "lua", "python", "typescript", "yaml", "zig" },
   },
 }
