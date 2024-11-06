8be63c52b875823e3354c842d672f85db55f6d5a
Lumynous <lumynou5.tw@gmail.com>
2024-11-06 12:29:44 +0800

    nvim: Update Lazy.nvim bootstrapping

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -24,17 +24,17 @@ vim.keymap.set({ "n" }, "<Leader>bc", "<Cmd>bdelete<CR>")
 
 -- Bootstrap Lazy.nvim.
 local lazy_path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazy_path) then
-	vim.fn.system({
+if not vim.uv.fs_stat(lazy_path) then
+	vim.system({
 		"git",
 		"clone",
 		"--filter=blob:none",
 		"--branch=stable",
 		"https://github.com/folke/lazy.nvim.git",
 		lazy_path,
-	})
+	}):wait()
 end
-vim.opt.rtp:prepend(lazy_path)
+vim.opt.runtimepath:prepend(lazy_path)
 
 require("lazy").setup("plugins", {
 	install = {
