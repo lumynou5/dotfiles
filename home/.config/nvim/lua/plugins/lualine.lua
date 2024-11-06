a739f0680ba23617a67aa0ee71c405f8bb58bc1d
Lumynous <lumynou5.tw@gmail.com>
2024-11-06 17:04:07 +0800

    nvim: Display virtual column for location instead

--- home/.config/nvim/lua/plugins/lualine.lua
+++ home/.config/nvim/lua/plugins/lualine.lua
@@ -20,7 +20,13 @@ return {
 			},
 			lualine_z = {
 				{ "progress" },
-				{ "location" },
+				{
+					function ()
+						local line = vim.fn.line(".")
+						local col = vim.fn.virtcol(".")
+						return string.format("%3d:%-2d", line, col)
+					end
+				},
 			},
 		},
 	},
