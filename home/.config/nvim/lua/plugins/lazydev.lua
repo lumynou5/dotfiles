7d84fa98417cf6661ff27a55324bb12aa63b76ad
Lumynous <lumynou5.tw@gmail.com>
2024-11-08 18:42:38 +0800

    nvim: Add vim.uv typing

--- home/.config/nvim/lua/plugins/lazydev.lua
+++ home/.config/nvim/lua/plugins/lazydev.lua
@@ -1,4 +1,26 @@
 return {
-	"folke/lazydev.nvim",
-	config = true,
+	{
+		"folke/lazydev.nvim",
+		opts = {
+			library = {
+				{
+					path = "luvit-meta/library",
+					words = { "vim%.uv" },
+				},
+			},
+		},
+	},
+	{
+		"Bilal2453/luvit-meta",
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		opts = function (_, opts)
+			opts.sources = opts.sources or {}
+			table.insert(opts.sources, {
+				name = "lazydev",
+				group_index = 0,
+			})
+		end,
+	},
 }
