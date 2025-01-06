d3f4cc91cf3b88500c828dc1611693b847e919d6
Lumynous <lumynou5.tw@gmail.com>
2025-01-06 12:45:52 +0800

    nvim: Add keymaps for gitsigns

--- home/.config/nvim/lua/plugins/gitsigns.lua
+++ home/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,4 +1,14 @@
 return {
 	"lewis6991/gitsigns.nvim",
-	config = true,
+	opts = {
+		preview_config = { border = "rounded" },
+	},
+	event = "BufEnter",
+	keys = {
+		{ "<Leader>hs", "<Cmd>Gitsigns stage_hunk<CR>", mode = "n" },
+		{ "<Leader>hu", "<Cmd>Gitsigns undo_stage_hunk<CR>", mode = "n" },
+		{ "<Leader>hr", "<Cmd>Gitsigns reset_hunk<CR>", mode = "n" },
+		{ "<Leader>hd", "<Cmd>Gitsigns diffthis<CR>", mode = "n" },
+		{ "<Leader>hb", "<Cmd>Gitsigns blame<CR>", mode = "n" },
+	},
 }
