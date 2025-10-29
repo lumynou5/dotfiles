199439c73394bfeed1a7e45e3b5a5ab333050131
Lumynous <lumynou5.tw@gmail.com>
2025-10-29 16:12:16 +0800

    nvim: Switch to oil.nvim

--- .config/nvim/lua/plugins/file_explorer.lua
+++ .config/nvim/lua/plugins/file_explorer.lua
@@ -1,37 +1,16 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
+	"stevearc/oil.nvim",
 	dependencies = {
-		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
 	},
 	opts = {
-		sources = {
-			"filesystem",
-			"document_symbols",
-		},
-		close_if_last_window = true,
-		enable_cursor_hijack = true,
-		source_selector = {
-			sources = {
-				{ source = "filesystem" },
-				{ source = "document_symbols" },
-			},
-		},
-		filesystem = {
-			filtered_items = {
-				hide_dotfiles = false,
-				never_show = {
-					".git",
-				},
-			},
+		float = {
+			max_width = 0.9,
+			border = "rounded",
 		},
 	},
-	cmd = {
-		"Neotree",
-	},
+	lazy = false,
 	keys = {
-		{ "<Leader>t", "<Cmd>Neotree<CR>" }
+		{ "<Leader>t", "<Cmd>Oil --float<CR>" }
 	},
 }
